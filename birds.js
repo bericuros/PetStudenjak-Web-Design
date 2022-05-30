6aef661881a76fe2fcba401d0b90ed3dfb997e04
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-05-30 16:45:31 +0200

    Initial commit

---
+++ birds.js
@@ -0,0 +1,136 @@
+$(document).ready(function() {
+    
+    let birds = []
+    let sort_type = null
+    let sort_direction = null
+    let search_by_name = null
+    let search_by_age = null
+
+
+    initialization()
+
+
+    function initialization() {
+        initializeObjects()
+        initializeLocalStorage()
+        printData()
+
+        $("#sort_button").on({click: function() {
+            $("#birds").remove()
+
+            sort_type = $("#sort_type").val()
+            sort_direction = parseInt($("#sort_direction").val())
+
+            sort_birds()
+
+            printData()
+        }})
+
+        $("#show_all_button").on({click: function() {
+            $("#birds").remove()
+
+            search_by_name = null
+            search_by_age = null
+
+            printData()
+        }})
+
+        $("#name_search").on({"keydown": function(e) {
+            if (e.key === "Enter" || e.keyCode === 13) {
+                if (document.getElementById("name_search").value != "") {
+                    search_by_name = document.getElementById("name_search").value
+                    search_by_age = null
+                    $("#birds").remove()
+                    printData()
+                }
+            }
+        }})
+
+        $("#age_search").on({"keydown": function(e) {
+            if (e.key === "Enter" || e.keyCode === 13) {
+                if (document.getElementById("age_search").value != "") {
+                    search_by_name = null
+                    search_by_age = parseInt(document.getElementById("age_search").value) * 12
+                    $("#birds").remove()
+                    printData()
+                }
+            }
+        }})
+    }
+
+
+    function initializeObjects() {
+        birds.push({
+            "name": "Parakeet LeBron",
+            "description": "Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna.",
+            "weight": 0.1,
+            "age": 27,
+            "link": "parakeet.html",
+        })
+        birds.push({
+            "name": "Cockatoo Chris",
+            "description": "Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna.",
+            "weight": 0.6,
+            "age": 12,
+            "link": "cockatoo.html",
+        })
+        birds.push({
+            "name": "Lovebird Dwyane",
+            "description": "Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna.",
+            "weight": 0.05,
+            "age": 50,
+            "link": "lovebird.html",
+        })
+
+        sort_type = $("#sort_type").val()
+        sort_direction = parseInt($("#sort_direction").val())
+
+        sort_birds()
+    }
+
+    function initializeLocalStorage() {
+        if ("localStorage" in window && window.localStorage !== null) {
+            ls_birds = localStorage.getItem("birds")
+            if (ls_birds == null) localStorage.setItem("birds", JSON.stringify(birds))
+            else birds = JSON.parse(localStorage.getItem("birds"))
+        }
+    }
+
+    function printData() {
+        let row = $("<div></div>").addClass("row").attr("id", "birds")
+        for (let i = 0; i < birds.length; i ++) {
+            if (search_by_name != null && birds[i]["name"].toLowerCase().indexOf(search_by_name.toLowerCase()) < 0) continue
+            if (search_by_age != null && !(search_by_age - 12 <= birds[i]["age"] && birds[i]["age"] <= search_by_age + 12)) continue
+
+            let col = $("<div></div>").addClass("offset-md-2").addClass("col-md-8").addClass("col-sm-12").addClass("text-left")
+            let link = $("<a></a>").addClass("link_dizajn_jedan").attr("href", birds[i]["link"])
+            let name = $("<h2></h2>").append(birds[i]["name"])
+            let paragraph = $("<p></p>").append(birds[i]["description"]).append("<br/>").append("Weight: ").
+                append(birds[i]["weight"]).append("kg").append("<br/>").append("Age: ").append((birds[i]["age"] - birds[i]["age"] % 12) / 12).
+                append("y").append(birds[i]["age"] % 12).append("m").append("<br/>")
+            let read_more_link = $("<a></a>").addClass("read_more").attr("href", birds[i]["link"]).append("See more")
+
+            paragraph.append(read_more_link)
+            link.append(name)
+            col.append(link)
+            col.append(paragraph)
+            row.append(col)
+        }
+        $("#body").append(row)
+    }
+
+    function compare(a, b) {
+        if (a[sort_type] < b[sort_type]) {
+            return sort_direction
+        }
+        else if (a[sort_type] > b[sort_type]) {
+            return -sort_direction
+        }
+        return 0
+    }
+
+    function sort_birds() {
+        birds.sort(compare)
+    }
+
+})
---
+++ dogs.js
@@ -0,0 +1,136 @@
+$(document).ready(function() {
+    
+    let dogs = []
+    let sort_type = null
+    let sort_direction = null
+    let search_by_name = null
+    let search_by_age = null
+
+
+    initialization()
+
+
+    function initialization() {
+        initializeObjects()
+        initializeLocalStorage()
+        printData()
+
+        $("#sort_button").on({click: function() {
+            $("#dogs").remove()
+
+            sort_type = $("#sort_type").val()
+            sort_direction = parseInt($("#sort_direction").val())
+
+            sort_dogs()
+
+            printData()
+        }})
+
+        $("#show_all_button").on({click: function() {
+            $("#dogs").remove()
+
+            search_by_name = null
+            search_by_age = null
+
+            printData()
+        }})
+
+        $("#name_search").on({"keydown": function(e) {
+            if (e.key === "Enter" || e.keyCode === 13) {
+                if (document.getElementById("name_search").value != "") {
+                    search_by_name = document.getElementById("name_search").value
+                    search_by_age = null
+                    $("#dogs").remove()
+                    printData()
+                }
+            }
+        }})
+
+        $("#age_search").on({"keydown": function(e) {
+            if (e.key === "Enter" || e.keyCode === 13) {
+                if (document.getElementById("age_search").value != "") {
+                    search_by_name = null
+                    search_by_age = parseInt(document.getElementById("age_search").value) * 12
+                    $("#dogs").remove()
+                    printData()
+                }
+            }
+        }})
+    }
+
+
+    function initializeObjects() {
+        dogs.push({
+            "name": "Dalmatian Buddy",
+            "description": "Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna.",
+            "weight": 25,
+            "age": 29,
+            "link": "dalmatian.html",
+        })
+        dogs.push({
+            "name": "Corgi Koko",
+            "description": "Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna.",
+            "weight": 12,
+            "age": 72,
+            "link": "corgi.html",
+        })
+        dogs.push({
+            "name": "Labrador Rocky",
+            "description": "Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum sot amet, consec teteur adipis cing elitsed diam non ummy nibh euismod tincidunt ut laoreet dolore magna.",
+            "weight": 30,
+            "age": 50,
+            "link": "labrador.html",
+        })
+
+        sort_type = $("#sort_type").val()
+        sort_direction = parseInt($("#sort_direction").val())
+
+        sort_dogs()
+    }
+
+    function initializeLocalStorage() {
+        if ("localStorage" in window && window.localStorage !== null) {
+            ls_dogs = localStorage.getItem("dogs")
+            if (ls_dogs == null) localStorage.setItem("dogs", JSON.stringify(dogs))
+            else dogs = JSON.parse(localStorage.getItem("dogs"))
+        }
+    }
+
+    function printData() {
+        let row = $("<div></div>").addClass("row").attr("id", "dogs")
+        for (let i = 0; i < dogs.length; i ++) {
+            if (search_by_name != null && dogs[i]["name"].toLowerCase().indexOf(search_by_name.toLowerCase()) < 0) continue
+            if (search_by_age != null && !(search_by_age - 12 <= dogs[i]["age"] && dogs[i]["age"] <= search_by_age + 12)) continue
+
+            let col = $("<div></div>").addClass("offset-md-2").addClass("col-md-8").addClass("col-sm-12").addClass("text-left")
+            let link = $("<a></a>").addClass("link_dizajn_jedan").attr("href", dogs[i]["link"])
+            let name = $("<h2></h2>").append(dogs[i]["name"])
+            let paragraph = $("<p></p>").append(dogs[i]["description"]).append("<br/>").append("Weight: ").
+                append(dogs[i]["weight"]).append("kg").append("<br/>").append("Age: ").append((dogs[i]["age"] - dogs[i]["age"] % 12) / 12).
+                append("y").append(dogs[i]["age"] % 12).append("m").append("<br/>")
+            let read_more_link = $("<a></a>").addClass("read_more").attr("href", dogs[i]["link"]).append("See more")
+
+            paragraph.append(read_more_link)
+            link.append(name)
+            col.append(link)
+            col.append(paragraph)
+            row.append(col)
+        }
+        $("#body").append(row)
+    }
+
+    function compare(a, b) {
+        if (a[sort_type] < b[sort_type]) {
+            return sort_direction
+        }
+        else if (a[sort_type] > b[sort_type]) {
+            return -sort_direction
+        }
+        return 0
+    }
+
+    function sort_dogs() {
+        dogs.sort(compare)
+    }
+
+})
